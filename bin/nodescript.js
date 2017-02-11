14d66e51a03000becd1eb92c5c094fee4c5815df
Todor Stoychev <pretodor@gmail.com>
2017-02-11 17:22:19 +0200

    Add 'nodescript' executable

---
+++ bin/nodescript.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+let babylon = require('nodescript-babylon')
+let babel = require('babel-core')
+let fs = require('fs')
+
+
+let fileName = process.argv[1]
+
+fs.readFile(fileName,  'utf8', function (err, data) {
+    if (err) { throw err }
+
+    let ast = babylon.parse(data, {plugins: ['si']}) // si: semicolon insertion
+    let newData = babel.transformFromAst(ast, data, {plugins: ['vdi']}).code // vdi: variable-declaration insertion
+    console.log(newData)
+})
